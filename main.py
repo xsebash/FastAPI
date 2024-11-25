90f80f245234ca9fdc11a63bb793398f1fa7bdbd
xsebash <151639054+xsebash@users.noreply.github.com>
2024-11-25 08:26:24 -0300

    Update main.py

--- main.py
+++ main.py
@@ -22,47 +22,47 @@ file_path = './MODELO.h5'
 #H
 model = load_model(file_path, compile=False)
 
- Abre el archivo
+#Abre el archivo
 with h5py.File(file_path, 'r') as f:
      Realiza operaciones con el archivo
     print(list(f.keys()))  # Por ejemplo, imprimir las claves del archivo
 
- Definir las clases
+#Definir las clases
 class_names = ['Normal', 'Anomalía']  # Cambia según tu contexto
 
 class ImageURL(BaseModel):
     url: str
 
 def preprocess_image(image):
-     Redimensionar la imagen a 256x256
+     #Redimensionar la imagen a 256x256
     image = image.resize((256, 256))
 
-     Convertir la imagen a RGB (asegúrate de que tenga 3 canales)
+     #Convertir la imagen a RGB (asegúrate de que tenga 3 canales)
     image = image.convert("RGB")
 
-     Convertir a un array de NumPy
+     #Convertir a un array de NumPy
     image_array = np.array(image)
 
-     Agregar una dimensión para el batch
+     #Agregar una dimensión para el batch
     image_array = np.expand_dims(image_array, axis=0)  # Ahora tiene forma (1, 256, 256, 3)
 
-     Normalizar el array
+     #Normalizar el array
     return image_array.astype('float32') / 255.0  # Normalizar a [0, 1]
 import requests
 
 @app.post("/analyze_image/")
 async def analyze_image(image: ImageURL):
     try:
-         Agrega un tiempo de espera
+         #Agrega un tiempo de espera
         response = requests.get(image.url, timeout=10)  # Tiempo de espera de 10 segundos
         if response.status_code != 200:
             #raise HTTPException(status_code=400, detail="No se pudo descargar la imagen")
 
-        Abrir la imagen y preprocesarla
+        #Abrir la imagen y preprocesarla
        img = Image.open(BytesIO(response.content))
         processed_image = preprocess_image(img)
 
-        Realizar la predicción
+        #Realizar la predicción
         prediction = model.predict(processed_image)
         predicted_class_index = np.argmax(prediction, axis=-1)[0]
         predicted_class = class_names[predicted_class_index]
